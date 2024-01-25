c32af088ffc437677205391c6232a098db449ac5
oleksandrsoldatenko23 <oleksandrsoldatenko23@gmail.com>
2024-01-25 20:28:26 +0200

    fixed imports

--- database/comments.py
+++ database/comments.py
@@ -1,7 +1,7 @@
 from flask_login import UserMixin
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
-from ..app.extensions import db
+from app.extensions import db
 
 class Comment(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
